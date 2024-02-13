437f6c25a7a297bef1ff46e44ae4e67e91c58db1
Pierre Comte <p.s.comte@hotmail.fr>
2024-02-13 18:19:31 +0100

    commit post soutenance

--- Debuggez-une-application-React.JS-main/src/containers/Slider/index.js
+++ Debuggez-une-application-React.JS-main/src/containers/Slider/index.js
@@ -28,18 +28,19 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div
-          key={event.id} // Clé unique pour chaque élément
-          className={`SlideCard SlideCard--${
-            index === idx ? "display" : "hide"
-          }`}
-        >
-          <img src={event.cover} alt="forum" />
-          <div className="SlideCard__descriptionContainer">
-            <div className="SlideCard__description">
-              <h3>{event.title}</h3>
-              <p>{event.description}</p>
-              <div>{getMonth(new Date(event.date))}</div>
+        <div key={event.title}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
             </div>
           </div>
         </div>
@@ -48,7 +49,7 @@ const Slider = () => {
         <div className="SlideCard__pagination">
           {byDateDesc.map((event, radioIdx) => (
             <input
-              key={event.id} // Utiliser un identifiant unique si possible
+              key={event.title} // Utiliser un identifiant unique si possible
               type="radio"
               name="radio-button"
               checked={index === radioIdx}
